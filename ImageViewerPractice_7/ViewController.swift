05a48795bcd9c5ed623aa7488d5819e3c53b91a2
derefaacline <cderefaa@gmail.com>
2023-02-14 12:49:28 -0600

    This is an application to view and pick images on your gallery

--- ImageViewerPractice_7/ViewController.swift
+++ ImageViewerPractice_7/ViewController.swift
@@ -7,13 +7,46 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UIImagePickerControllerDelegate & UINavigationControllerDelegate {
 
+    @IBOutlet weak var ImageView: UIImageView!
+    lazy var ImPicker = UIImagePickerController()
+    let alertmessage = "Please pick an image"
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        setupPicker()
+    }
+    func setupPicker(){
+        ImPicker.delegate = self
+        ImPicker.videoQuality = UIImagePickerController.QualityType.typeMedium
     }
 
-
+    @IBAction func Tapme(_ sender: Any) {
+        present(ImPicker, animated: true)
+    }
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        guard let img = info[UIImagePickerController.InfoKey.originalImage] as? UIImage else { return }
+        dismiss(animated: true){
+            self.ImageView.image = img
+        }
+    }
+    func showAlert() {
+        let alert = UIAlertController.init(title: "Warning", message: alertmessage, preferredStyle: UIAlertController.Style.alert)
+        let alertAction =  UIAlertAction.init(title: "OK", style: .default){ _ in
+            alert.dismiss(animated: true, completion: nil)
+        }
+        alert.addAction(alertAction)
+        present(alert, animated: true)
+        
+    }
+    
+    func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
+        dismiss(animated: true){
+            self.showAlert()
+        }
+    }
+    
 }
 
